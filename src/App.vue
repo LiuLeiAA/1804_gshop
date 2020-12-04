5af89052462c1e3c06d0a24f8d60487718d6e744
LiuLeiAA <LiuLeiAA@email.com>
2020-12-04 09:14:52 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+<!--      显示路由组件-->
+      <router-view/>
+      <FooterGuide v-show="$route.meta.showFooter"/>
+    </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+export default {
+    //映射成标签
+    components : {
+      FooterGuide
+    }
+}
+</script>
+<!--声明 告诉 WebStorm 里面写的是什么语法
+应用根组件 vue
+-->
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .app
+    width 100%
+    height 100%
+    background #f5f5f5
+</style>
